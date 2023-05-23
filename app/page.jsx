e76f1f35a6f6b88aa3fbfc966093ce29aaac3086
shobhit <shobhitmishra2002@gmail.com>
2023-05-23 16:06:12 +0530

    added first task for tushar

---
+++ app/page.jsx
@@ -0,0 +1,10 @@
+import SmallCard from "@/components/cards/SmallCard";
+
+export default function Home() {
+  return (
+    <div>
+      home page
+      <SmallCard />
+    </div>
+  );
+}
---
+++ components/cards/SmallCard.jsx
@@ -0,0 +1,12 @@
+import "./smallCard.css";
+
+/*
+So brother your task is to create a small card as discussed.
+you can edit this file in order to develop a nice card and it has an attached css file smallCard.css
+*/
+
+const SmallCard = () => {
+  return <div className="heading">small card</div>;
+};
+
+export default SmallCard;
