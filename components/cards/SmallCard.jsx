9f6d324a381070f9f103328d6405045ff5f13f3a
shobhit <shobhitmishra2002@gmail.com>
2023-05-24 11:14:17 +0530

    refactored the small scard component

--- components/cards/SmallCard.jsx
+++ components/cards/SmallCard.jsx
@@ -1,20 +1,18 @@
 import "./smallCard.css";
 
-
 const SmallCard = () => {
   return (
     <div className="smallCard">
-
-      <div className="smallCardImg">
-        <img src="#" alt="smallCardImg"/>
-      </div>
+      <img className="smallCardImg" src="/tShirt.png" alt="smallCardImg" />
 
       <div className="smallCardAbout">
         <div className="smallCardCost">$10.00</div>
-        <div className="smallCardDescription">T-shirts with multiple colors, for men</div>
+        <div className="smallCardDescription">
+          T-shirts with multiple colors, for men
+        </div>
       </div>
     </div>
-  )
+  );
 };
 
 export default SmallCard;
