4e855d9c85fa26cefb3ab02fe7bba7054f4e401d
shobhit <shobhitmishra2002@gmail.com>
2023-05-29 19:10:26 +0530

    added sales or deals section

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,7 @@ import SmallCard from "@/components/cards/SmallCard";
 import Header from "@/components/header/Header";
 import HorizontalCards from "@/components/horizontalCards/horizontalCards";
 import Recommendation from "@/components/recommend/Recommendation";
+import SaleSection from "@/components/sale/SaleSection";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       {/* <SmallCard /> */}
       {/* <HorizontalCards /> */}
       <Header />
+      <SaleSection />
       <Recommendation />
     </div>
   );
---
+++ components/sale/SaleSection.jsx
@@ -0,0 +1,56 @@
+import "./saleSection.css";
+
+const Card = () => (
+  <div className="sale_card">
+    <img src="/tShirt.png" alt="image" />
+
+    <h5>T-shirt</h5>
+    <div>
+      <span>-25%</span>
+    </div>
+  </div>
+);
+
+const SaleSection = () => {
+  return (
+    <div className="sale container">
+      <div className="sale_clock">
+        <div className="sale_clock__heading">
+          <h4>Deals and offers</h4>
+          <h5>Electronic equipments</h5>
+        </div>
+        <div className="clock_section">
+          <div className="clock_section__item">
+            <span className="clock_item__first">6</span>
+            <span className="clock_item__second">Days</span>
+          </div>
+          <div className="clock_section__item">
+            <span className="clock_item__first">13</span>
+            <span className="clock_item__second">Hour</span>
+          </div>
+          <div className="clock_section__item">
+            <span className="clock_item__first">34</span>
+            <span className="clock_item__second">Min</span>
+          </div>
+          <div className="clock_section__item">
+            <span className="clock_item__first">13</span>
+            <span className="clock_item__second">Sec</span>
+          </div>
+        </div>
+      </div>
+
+      <div className="sale_cards">
+        <div className="sale_cards__window">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SaleSection;
