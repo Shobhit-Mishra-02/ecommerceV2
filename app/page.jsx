5e50ba6b433cd20c1abe166bb5a5fdb9f5263c61
shobhit <shobhitmishra2002@gmail.com>
2023-05-31 11:17:28 +0530

    added some changes in navigation section

--- app/page.jsx
+++ app/page.jsx
@@ -8,9 +8,9 @@ export default function Home() {
   return (
     <div>
       {/* <SmallCard /> */}
+      {/* <HorizontalCards /> */}
       <Header />
       <SaleSection />
-      <HorizontalCards />
       <Recommendation />
     </div>
   );
