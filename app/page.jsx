d9c0598f8dc3789c7ecb8bbdd29f5ee7c86ace82
Tushar Malhotra <malhotratushar999@gmail.com>
2023-06-01 11:29:57 +0530

    sectionInquiry

--- app/page.jsx
+++ app/page.jsx
@@ -4,15 +4,17 @@ import HorizontalCards from "@/components/horizontalCards/horizontalCards";
 import Recommendation from "@/components/recommend/Recommendation";
 import SaleSection from "@/components/sale/SaleSection";
 import Newsletter from "@/components/newsletter/newsletter";
+import SectionInquiry from "@/components/sectionInquiry/sectionInquiry";
 
 
 export default function Home() {
   return (
     <div>
       {/* <SmallCard /> */}
+      {/* <HorizontalCards /> */}
       <Header />
       <SaleSection />
-      <HorizontalCards />
+      <SectionInquiry />
       <Recommendation />
       <Newsletter />
     </div>
